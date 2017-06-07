5d4ab9225659d0236c8a2ee0663797f2957b6315
tuyennc <tuyennc@k.s-cubism.jp>
2017-06-07 17:37:04 +0700

    Done code home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
             <div class="row">
                 <div class="col-sm-8">
                     <div class="logo pull-left">
-                        <a href="#"><img src="images/home/logo.png" alt="" /></a>
+                        <a href="#"><img src="images/home/camera-benjamin-gia-re-3.jpg" alt="" /></a>
                     </div>
                     <div class="benjamin-search">
                         <form method="GET" onsubmit="">
@@ -94,9 +94,16 @@
                             <a href="#"><i class="fa fa-bars"></i> DANH MỤC SẢN PHẨM</a>
                         </div>
                         <div class="categories-list-box">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                            <ul>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/lap-dat-camera-tron-bo.png"><span class="mc_title"><strong>Camera trọn bộ</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/camera-giam-sat.png"><span class="mc_title"><strong>Camera giám sát</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/dau-ghi-hinh-camera-quan-sat.png"><span class="mc_title"><strong>Đầu ghi hình camera</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/phu-kien-camera.png"><span class="mc_title"><strong>Phụ kiện camera</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/camera-thiet-bi-an-ninh.png"><span class="mc_title"><strong>Thiết bị an ninh</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/may-cham-cong-gia-re.png"><span class="mc_title"><strong>Máy chấm công</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/tong-dai-bo-dam.png"><span class="mc_title"><strong>Tổng đài - Bộ đàm</strong></span></a></li>
+                                <li><a href="#"><img src="images/danh-muc-san-pham/thiet-bi-van-phong.png"><span class="mc_title"><strong>Thiết bị văn phòng</strong></span></a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -118,13 +125,9 @@
     <div class="footer-top">
         <div class="container">
             <div class="row">
-                <div class="col-sm-2">
-                    <div class="companyinfo">
-                        <h2><span>Benjamin</span></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
-                    </div>
-                </div>
+
                 <div class="col-sm-7">
+
                     <div class="col-sm-3">
                         <div class="video-gallery text-center">
                             <a href="#">
@@ -188,7 +191,7 @@
                 <div class="col-sm-3">
                     <div class="address">
                         <img src="images/home/map.png" alt="" />
-                        <p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
+                        <p>26 Nguyễn Văn Thạnh, Long Thạnh Mỹ, Quận 9, TP HCM</p>
                     </div>
                 </div>
             </div>
@@ -198,65 +201,167 @@
     <div class="footer-widget">
         <div class="container">
             <div class="row">
-                <div class="col-sm-2">
-                    <div class="single-widget">
-                        <h2>Service</h2>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Online Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Order Status</a></li>
-                            <li><a href="#">Change Location</a></li>
-                            <li><a href="#">FAQ’s</a></li>
+                <div class="col-xs-12 col-md-4 col-lg-3">
+                    <div class="social-claim">
+                        <div class="claim_header">Cam kết</div>
+                        <div class="claim">Sản phẩm hàng hóa chính hãng, đa dạng, phong phú</div>
+                        <div class="claim">Luôn luôn giá rẻ và khuyến mại không ngừng</div>
+                        <div class="claim">Dịch vụ chăm sóc khách hàng uy tín, tận tâm</div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4 col-lg-3">
+                    <div class="social-claim2">
+                        <div class="claim_header">Hỗ trợ khách hàng</div>
+                        <ul>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Xem camera giám sát qua điện thoại</a></li>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Xem camera quan sát qua máy tính</a></li>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Download tài liệu</a></li>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">HD cài đặt phần mềm Teamviewer</a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-2">
-                    <div class="single-widget">
-                        <h2>Quock Shop</h2>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">T-Shirt</a></li>
-                            <li><a href="#">Mens</a></li>
-                            <li><a href="#">Womens</a></li>
-                            <li><a href="#">Gift Cards</a></li>
-                            <li><a href="#">Shoes</a></li>
+                <div class="col-xs-12 col-md-4 col-lg-3">
+                    <div class="social-claim2">
+                        <div class="claim_header">Thông tin công ty</div>
+                        <ul>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Giới thiệu</a></li>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Tuyển dụng</a></li>
+                            <li><img src="images/camera-footer/list-style.png"><a href="#">Chính sách</a></li>
                         </ul>
+                        <div id="social_us">
+                            <div class="social_us_header">Kết nối với chúng tôi</div>
+                            <div id="f_social">
+                                <a target="_blank" rel="nofollow" href="https://www.facebook.com/chituyen.nguyen.98">
+                                    <img src="images/camera-footer/f_fb.png">
+                                </a>
+                                <a target="_blank" rel="nofollow" href="#">
+                                    <img src="images/camera-footer/f_gplus.png">
+                                </a>
+                                <a target="_blank" rel="nofollow" href="#">
+                                    <img src="images/camera-footer/f_yt.png">
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-2">
-                    <div class="single-widget">
-                        <h2>Policies</h2>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Privecy Policy</a></li>
-                            <li><a href="#">Refund Policy</a></li>
-                            <li><a href="#">Billing System</a></li>
-                            <li><a href="#">Ticket System</a></li>
-                        </ul>
+                <div class="col-xs-12 col-md-4 col-lg-3 hidden-md">
+                    <div class="social-claim2">
+                        <div class="claim_header">Tổng đài trợ giúp</div>
+                        <div style="margin: 10px;">
+                            <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
+                                <tbody>
+                                <tr>
+                                    <td><img src="images/camera-footer/tro-giup.png" style="width: 68px; height: 81px;"></td>
+                                    <td style="vertical-align: middle;">
+                                        <p><span style="color:#333333;"><span style="font-size:14px;">Tư vấn khách hàng</span></span></p>
+                                        <p><span style="color:#ba0000;"><span style="font-size:20px;"><strong>0120 4588 241</strong></span></span></p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <hr style="color: #000;">
+                                        <span style="color:#555555;"><span style="font-size:13px;"><img src="images/camera-footer/life_saver.png">&nbsp;Chăm sóc và hỗ trợ sau bán hàng</span></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" style="text-align: center;">
+                                        <span style="color:#B22222;"><strong style="font-size:16px;">0903 338 011</strong></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <span style="font-size:13px;"><span style="color:#555555;"><img src="images/camera-footer/mailer.png">&nbsp;benja.vn@gmail.com</span></span>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-2">
-                    <div class="single-widget">
-                        <h2>About Shopper</h2>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Company Information</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">Store Location</a></li>
-                            <li><a href="#">Affillate Program</a></li>
-                            <li><a href="#">Copyright</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-sm-3 col-sm-offset-1">
-                    <div class="single-widget">
-                        <h2>About Shopper</h2>
-                        <form action="#" class="searchform">
-                            <input type="text" placeholder="Your email address" />
-                            <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-                            <p>Get the most recent updates from <br />our site and be updated your self...</p>
-                        </form>
-                    </div>
-                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="container-fluid bg-footer">
+        <div class="row">
+            <div class="col-xs-12 col-xs-offset-0 col-md-10 col-md-offset-1">
+                <div class="row">
+                    <div class="col-md-2 hidden-xs hidden-sm">
+                        <div class="f-logo">
+                            <a href="#">
+                                <img title="LẮP ĐẶT CAMERA TRỌN BỘ GIÁ RẺ" alt="Benjamin Camera Việt Nam tự hào Nhà cung cấp thiết bị camera giám sát giá rẻ uy tín trên toàn quốc" src="images/home/camera-benjamin-gia-re-2.jpg">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-5">
+                        <div class="f-address">
+                            <p>
+                                <span style="color:#ff6600;"><strong>LẮP ĐẶT CAMERA&nbsp;DỊCH VỤ&nbsp;BENJA&nbsp;&nbsp;VIỆT NAM TẠI HÀ NỘI</strong></span>
+                                <span style="color:#FF8C00;"><strong>&nbsp;</strong></span><br>
+                                Địa chỉ:
+                                <span style="color:#FFD700;">Số 12 Ngõ 18/46 Định Công Thượng, Q.Hoàng Mai, TP. Hà Nội</span><br>
+                                Điện thoại:
+                                <span style="color:#FFD700;">&nbsp;0903 338 011&nbsp;-&nbsp;0120 4588 241</span><br>
+                                Website:
+                                <span style="color:#FFD700;">http://benja-camera.vn</span>
+                            </p>
+                            <p>
+                                <span style="color:#ff6600;"><strong>LẮP ĐẶT CAMERA&nbsp;BENJA&nbsp;&nbsp;CHI NHÁNH TP.HCM</strong></span>
+                                <span style="color:#FF8C00;"><strong>&nbsp;</strong></span><br>
+                                Địa chỉ:
+                                <span style="color:#FFD700;">Số 26 Nguyễn Văn Thạnh, P.Long Thạnh Mỹ, Quận 9, TP.HCM</span><br>
+                                Điện thoại:
+                                <span style="color:#FFD700;">&nbsp;0903 338 011&nbsp;-&nbsp;0120 4588 241</span><br>
+                                Website:
+                                <span style="color:#FFD700;">http://benja-camera.vn</span>
+                            </p>
+                            <p>&nbsp;</p>
+                            <p>
+                                <span style="color:#FF8C00;">Giới thiệu dịch vụ lắp đặt camera của Benja Việt Nam</span>
+                            </p>
+                            <p>
+                                <em>
+                                    Trước nhu cầu của thị trường và mở rộng ngành dịch vụ Công ty Benja Camera Việt Nam được sáng lập hoạt động kết hợp với Tập đoàn BENJAMIN triển khai lắp đặt Internet Cáp quang – Camera giám sát tại&nbsp;khắp các tỉnh thành cả nước.
+                                    "Hiện chúng tôi đang phát triển kết hợp với một số <strong>đơn vị lắp đặt camera uy tín</strong> tạo nên một thị trường dịch vụ công nghệ uy tín trên toàn quốc".
+                                </em>
+                            </p>
+                            <p>&nbsp;</p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-5">
+                        <div class="f-address">
+                            <p>
+                                <strong >
+                                    <span style="color:#FF8C00;">
+                                        Trụ sở&nbsp;BENJAMIN VN Inc
+                                    </span>
+                                </strong>
+                                <br style="font-size: 14px; line-height: 22.3999996185303px;">
+                                <span style="font-size: 14px; line-height: 22.3999996185303px;">
+                                    Địa chỉ:
+                                    <span style="color:#FFD700;">KCN Quận 9, TP.HCM</span><br>
+                                    Điện thoại:
+                                    <span style="color:#FFD700;">0903&nbsp;338 011</span>
+                                </span>
+                            </p>
+                            <p>
+                                <span style="color:#FF8C00;">Hiện chúng tôi có các điểm dịch vụ như: </span>
+                            </p>
+                            <p>
+                                <a href="#">
+                                    <span style="color:#cccccc;">Lắp đặt camera tại các quận huyện thuộc TP.HCM và các tỉnh thành</span>
+                                    <span style="color:#cccccc;">Lắp đặt camera tại các quận huyện thuộc Hà Nội và các tỉnh thành ...</span><br>
+                                    <span style="color:#cccccc;">Quý khách cần lắp đặt camera, Internet vui lòng liên hệ để được tư vấn lắp đặt. </span>
+                                </a>
+                            </p>
+                            <p>&nbsp;</p>
+                            <p style="text-align: right;">
+                                <img alt="Benja camera đã đăng ký bộ công thương" src="images/home/benja-camera-thong-bao-bo-cong-thuong.png">
+                            </p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
             </div>
         </div>
     </div>
